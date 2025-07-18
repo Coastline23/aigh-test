1f79d4dd8a4f956f051e388f8adc5d1092cbccc2
Coastline23 <43003230+Coastline23@users.noreply.github.com>
2025-07-18 17:10:45 +0100

    Create __init__.py

---
+++ client_code/Form1/RowTemplate1/__init__.py
@@ -0,0 +1,27 @@
+from ._anvil_designer import RowTemplate1Template
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class RowTemplate1(RowTemplate1Template):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
+
+    def edit_button_click(self, **event_args):
+        """Populate Form1 inputs for editing"""
+        main_form = get_open_form()
+        main_form.text_name.text = self.item['name']
+        main_form.text_email.text = self.item['email']
+        main_form.text_mobile.text = self.item['mobile']
+        main_form.current_row = self.item
+        main_form.save_button.text = "Update Client"
+
+    def delete_button_click(self, **event_args):
+        """Delete the row"""
+        if confirm("Are you sure you want to delete this client?"):
+            self.item.delete()
+            get_open_form().refresh_clients()
